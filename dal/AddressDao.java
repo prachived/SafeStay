9ecc9ffe9276e019036b1b2d0a2d4c1ca5be9d1c
Grishma Thakkar <34215677+grishmathakkar@users.noreply.github.com>
2018-03-16 02:27:30 -0400

    Create AddressDao.java

---
+++ dal/AddressDao.java
@@ -0,0 +1,216 @@
+package StaySafe.dal;
+import java.sql.SQLException;
+import java.util.*;
+
+import StaySafe.model.*;
+public class AddressDao {
+protected ConnectionManager connectionManager;
+
+	
+
+	// Single pattern: instantiation is limited to one object.
+
+	private static AddressDao instance = null;
+
+	protected AddressDao() {
+
+		connectionManager = new ConnectionManager();
+
+	}
+
+	public static AddressDao getInstance() {
+
+		if(instance == null) {
+
+			instance = new AddressDao();
+
+		}
+
+		return instance;
+
+	}
+	public Address create(Address address) throws SQLException {
+		//Grishma Thakkar
+		String createAddress = "INSERT INTO Address(Location,Street,FirstName, Longitude, Latitude) VALUES(?,?,?,?);";
+
+		Connection connection = null;
+
+		PreparedStatement insertStmt = null;
+
+		try {
+
+			connection = connectionManager.getConnection();
+
+			insertStmt = connection.prepareStatement(createAddress);
+
+			insertStmt.setString(1, address.getLocation());
+
+			insertStmt.setString(2, address.getStreet());
+
+			insertStmt.setString(3, address.getLongitude());
+
+			insertStmt.setString(4, address.getLatitude());
+
+
+			insertStmt.executeUpdate();
+
+			return address;
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+			throw e;
+
+		}
+
+		finally {
+
+			if(connection != null) {
+
+				connection.close();
+
+			}
+
+			if(insertStmt != null) {
+
+				insertStmt.close();
+
+			}
+
+		}
+
+	}
+	public Address getAddressByLocation(String location) throws SQLException {
+
+		String selAddress = "SELECT * FROM Address WHERE location=?;";
+
+		Connection connection = null;
+
+		PreparedStatement selectStmt = null;
+
+		ResultSet results = null;
+
+		try {
+
+			connection = connectionManager.getConnection();
+
+			selectStmt = connection.prepareStatement(selAddress);
+
+			selectStmt.setString(1, location);
+
+			// Note that we call executeQuery(). This is used for a SELECT statement
+
+			// because it returns a result set. For more information, see:
+
+			// http://docs.oracle.com/javase/7/docs/api/java/sql/PreparedStatement.html
+
+			// http://docs.oracle.com/javase/7/docs/api/java/sql/ResultSet.html
+
+			results = selectStmt.executeQuery();
+
+			// You can iterate the result set (although the example below only retrieves 
+
+			// the first record). The cursor is initially positioned before the row.
+
+			// Furthermore, you can retrieve fields by name and by type.
+
+			if(results.next()) {
+
+				String rlocation = results.getString("Location");
+
+				String rstreet = results.getString("Street");
+
+				String rlatitude = results.getString("Latitude");
+
+				String rlongitude = results.getString("Longitude");
+
+				Address address = new Address(rlocation, rstreet, rlongitude, rlatitude);
+
+				return address;
+
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+			throw e;
+
+		} finally {
+
+			if(connection != null) {
+
+				connection.close();
+
+			}
+
+			if(selectStmt != null) {
+
+				selectStmt.close();
+
+			}
+
+			if(results != null) {
+
+				results.close();
+
+			}
+
+		}
+
+		return null;
+
+	}
+
+	
+	public Address delete(Address address) throws SQLException {
+
+		String delAddress = "DELETE FROM Address WHERE location=?;";
+
+		Connection connection = null;
+
+		PreparedStatement deleteStmt = null;
+
+		try {
+
+			connection = connectionManager.getConnection();
+
+			deleteStmt = connection.prepareStatement(delAddress);
+
+			deleteStmt.setString(1, address.getLocation());
+			int affectedRows = deleteStmt.executeUpdate();
+			if (affectedRows == 0) {
+				throw new SQLException("No records available");
+			}
+
+
+
+			// Return null so the caller can no longer operate on the Users instance.
+
+			return null;
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+			throw e;
+
+		} finally {
+
+			if(connection != null) {
+
+				connection.close();
+
+			}
+
+			if(deleteStmt != null) {
+
+				deleteStmt.close();
+
+			}
+
+		}
+
+	}
+}
