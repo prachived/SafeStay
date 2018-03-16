3c55e79860d3b99b35c096a0b6f73ec6ca98b7fc
Grishma Thakkar <34215677+grishmathakkar@users.noreply.github.com>
2018-03-16 00:01:11 -0400

    Create Recommendations.java

---
+++ Recommendations.java
@@ -0,0 +1,86 @@
+package StaySafe.model;
+
+public class Recommendations {
+		protected int recommendationId;
+		protected EndUsers endusers;
+		protected Address address;
+		protected double rating;
+		protected double petfriendly;
+		protected double childfriendly;
+		
+		
+
+		public Recommendations(EndUsers endusers, Address address, double rating, double petfriendly, double childfriendly ) {
+			//Grishma Thakkar
+			this.endusers = users;
+			this.address = address;
+			this.childfriendly=childfriendly;
+			this.petfriendly=petfriendly;
+		    this.rating=rating;
+		}
+
+		public Recommendations(int recommendationId) {
+			this.recommendationId = recommendationId;
+		}
+
+		public Recommendations(int recommendationId, EndUsers endusers, Address address, double rating, double petfriendly, double childfriendly ) {
+			this.recommendationId = recommendationId;
+			this.Endusers = endusers;
+			this.address = address;
+			this.childfriendly=childfriendly;
+			this.petfriendly=petfriendly;
+		    this.rating=rating;
+		}
+		
+		public EndUsers getEndusers() {
+			return endusers;
+		}
+
+		public void setEndusers(EndUsers endusers) {
+			this.endusers = endusers;
+		}
+
+		public Address getAddress() {
+			return address;
+		}
+
+		public void setAddress(Address address) {
+			this.address = address;
+		}
+
+		public double getRating() {
+			return rating;
+		}
+
+		public void setRating(float rating) {
+			this.rating = rating;
+		}
+
+		public double getPetfriendly() {
+			return petfriendly;
+		}
+
+		public void setPetfriendly(float petfriendly) {
+			this.petfriendly = petfriendly;
+		}
+
+		public double getChildfriendly() {
+			return childfriendly;
+		}
+
+		public void setChildfriendly(float childfriendly) {
+			this.childfriendly = childfriendly;
+		}
+
+		public int getRecommendationId() {
+			return recommendationId;
+		}
+
+		public void setRecommendationId(int recommendationId) {
+			this.recommendationId = recommendationId;
+		}
+
+	
+
+	}
+
